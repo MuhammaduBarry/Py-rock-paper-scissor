8527994f256be129b675ab04317d552ab4edf49d
Muhammadu Barry <muhammadubarry5@gmail.com>
2024-02-22 09:26:58 -0500

    fixed bug

--- main.py
+++ main.py
@@ -17,18 +17,18 @@ def game():
     ''')
 
     # Creating a while loop to check if player's move is in choices
-    i = 0
-    while True and i < 5:
+    count = 0
+    while True and count < 5:
         players_move = input("Choose your weapon: ").lower()
         if players_move in choices:
             print(f"Good job {player_name}! {players_move} is a valid weapon.")
+            count += 1
         else:
             print(f'''
                    You fool, {players_move} is not a weapon,
                    you are going to cause humanity to lose,
                    TRY AGAIN!!!!!
                ''')
-        i += 1
 
 
 game()
